cf55103615946ad327ac7bab3e9a7b7d2fb73c04
sonv-desktop <van.truongson@gmaill.com>
2024-07-04 21:20:16 +0700

    update

--- init.lua
+++ init.lua
@@ -795,6 +795,9 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    opts = {
+      transparent = true,
+    },
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
