cd0ff902d8714d8cba87a53ca15f269661a3e2da
Son Van <van.truongson@gmail.com>
2025-03-28 21:33:30 +0700

    update

--- init.lua
+++ init.lua
@@ -276,6 +276,31 @@ require('lazy').setup({
   {
     'sindrets/diffview.nvim',
   },
+
+  -- Installing snacks to preview live
+  {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+},
   -- Installing vimtex for LaTeX
   {
     'lervag/vimtex',
